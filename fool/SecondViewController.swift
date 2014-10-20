0fad0ab9ffb5e4aa9eaf0946877572b923ecdccc
Jerry Jiang <leeming.jiang@gmail.com>
2014-10-20 13:49:38 +0800

    Initial Commit

---
+++ fool/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  fool
+//
+//  Created by Jerry Jiang on 14/10/20.
+//  Copyright (c) 2014年 Jerry Jiang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
